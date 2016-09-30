6823be8d8012f820521c64a8e6abb81b0211e713
CollinBradford <cbsoftwareandengineering@gmail.com>
2016-09-30 11:41:50 -0500

    Created ClockLatcher Modle
    Create ClockLatcher Module.  This module will be used to latch the
    clocks automatically at startup.  It should be used in conjunction with
    the slow 15 mhz clock to give the faster clocks some time to normalize.
    The module has been tested and is ready for deployment.

--- GEL_CAPTAN/fuseRelaunch.cmd
+++ GEL_CAPTAN/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "secureip" -o "D:/cbradford/WorkingExampleCollinDebug/GPS_ADC_/GEL_CAPTAN/PeakFinder_tb_isim_beh.exe" -prj "D:/cbradford/WorkingExampleCollinDebug/GPS_ADC_/GEL_CAPTAN/PeakFinder_tb_beh.prj" "work.PeakFinder_tb" 
+-intstyle "ise" -incremental -lib "secureip" -o "D:/cbradford/NewADCFirmware/GEL_CAPTAN/ClockLatchSignals_tb_isim_beh.exe" -prj "D:/cbradford/NewADCFirmware/GEL_CAPTAN/ClockLatchSignals_tb_beh.prj" "work.ClockLatchSignals_tb" 
